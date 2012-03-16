6b546d8b984a1484b0d77d8fbcb34781b00d168b
Rico Sta. Cruz <rico@ambiescent.com>
2012-03-16 14:14:31 +0800

    chore: initial import

---
+++ _js/behaviors/no-preview.js
@@ -0,0 +1,14 @@
+import remove from 'dom101/remove'
+import onmount from 'onmount'
+
+import { isPreview } from '../helpers/preview'
+
+/*
+ * Behavior: Things to remove when preview mode is on
+ */
+
+onmount('[data-js-no-preview]', function (b) {
+  if (isPreview()) {
+    remove(this)
+  }
+})
